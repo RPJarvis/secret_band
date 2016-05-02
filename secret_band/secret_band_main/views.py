972d2f1cb4958c2a063fde42ed9e827369f7b53e
JARVIS <RJarvis1@cnm.edu>
2016-05-02 11:00:59 -0600

    templates

--- secret_band/secret_band_main/views.py
+++ secret_band/secret_band_main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = RequestContext(request)
+
+    return render_to_response('the_breaktone_main/index.html', {}, context)
+
+
+def listen(request):
+    context = RequestContext(request)
+
+    return render_to_response('wild_humans_main/listen.html', {}, context)
